21080b3adeaaa34d817eaf5d09755fc371659d87
BtcDrak <btcdrak@gmail.com>
2014-09-22 11:11:03 +0100

    Added notary APIs

--- js/util.api.js
+++ js/util.api.js
@@ -197,6 +197,7 @@ function _getDestTypeFromMethod(method) {
       'get_owned_assets', 'get_asset_history', 'get_asset_extended_info', 'get_transaction_stats', 'get_wallet_stats', 'get_asset_pair_market_info',
       'get_market_price_summary', 'get_market_price_history', 'get_market_info', 'get_market_info_leaderboard', 'get_market_cap_history',
       'get_order_book_simple', 'get_order_book_buysell', 'get_trade_history',
+      'get_documents_for', 'get_document_state_for',
       'get_bets', 'get_user_bets', 'get_feed', 'get_feeds_by_source',
       'parse_base64_feed', 'get_open_rps_count', 'get_user_rps', 
       'get_users_pairs', 'get_market_orders', 'get_market_trades', 'get_markets_list', 'get_market_details',
